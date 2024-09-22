6deb02e78d34bd79b3c372ee4ef89390246396fa
ramyus889 <ramyus889.com>
2024-09-22 17:10:47 +0300

    fix drawer

--- src/components/UseDrawer.vue
+++ src/components/UseDrawer.vue
@@ -8,9 +8,11 @@ const visibleRight = ref(false);
   <div class="card flex justify-center">
     <Drawer v-model:visible="visibleRight" header="Illa Cloud Pricing" position="right">
       <div class="flex flex-col gap-4">
-        <RouterLink to="/" class="">Home</RouterLink>
-        <RouterLink to="/illa-flow" class="">ILLA Flow</RouterLink>
-        <RouterLink to="/integrations" class="">Integrations</RouterLink>
+        <RouterLink to="/" @click="visibleRight = false" class="">Home</RouterLink>
+        <RouterLink to="/illa-flow" @click="visibleRight = false" class="">ILLA Flow</RouterLink>
+        <RouterLink to="/integrations" @click="visibleRight = false" class=""
+          >Integrations</RouterLink
+        >
       </div>
     </Drawer>
     <Button icon="pi pi-arrow-left" severity="secondary" @click="visibleRight = true" />
